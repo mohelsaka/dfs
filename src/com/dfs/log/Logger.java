26c7402fd52a1831ec59dcf3af17f47c0b96fa51
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2013-05-09 10:18:31 +0200

    commenting logger class

--- src/com/dfs/log/Logger.java
+++ src/com/dfs/log/Logger.java
@@ -15,6 +15,9 @@ public class Logger {
 	private static final String LOG_FILE_PATH = "log";
 	private static BufferedWriter log;
 	
+	/**
+	 * Initilaizes the logger and opening stream on log file or create it
+	 * */
 	public static void init(){
 		try {
 			File logFIle = new File(LOG_FILE_PATH);
@@ -29,15 +32,33 @@ public class Logger {
 		}
 	}
 	
+	/**
+	 * printing transaction information to the log
+	 * 
+	 * @param	tx	transaction to be logged
+	 * */
 	public static void logTransaction(Transaction tx){
 		String msg = String.format("%d:%d:%s", tx.getId(), tx.getState(), tx.getFileName());
 		writeLogEntry(TRANSACTION_LOG_ENTRY, msg);
 	}
 	
+	/**
+	 * printing log entry when file is read
+	 * 
+	 * @param fileName	name of the file that is being read 
+	 * */
 	public static void logReadFile(String fileName){
 		writeLogEntry(READ_LOG_ENTRY, fileName);
 	}
 	
+	
+	/**
+	 * printing a log entry for a transaction message
+	 * 
+	 * @param	txnid	id transaction
+	 * @param	msgid	id of the message that has been writen
+	 * @param	msgSize	size of the message that has been writen
+	 * */
 	public static void logWriteRequest(long txnid, long msgid, long msgSize){
 		String msg = String.format("%d:%d:%d", txnid, msgid, msgSize);
 		writeLogEntry(WRITE_LOG_ENTRY, msg);
