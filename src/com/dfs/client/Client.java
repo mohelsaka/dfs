d17113c3db4ad17ceda93535f38c12079b8065cd
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2013-05-13 21:32:48 +0200

    implmentation provided for function updateServerIP

--- src/com/dfs/client/Client.java
+++ src/com/dfs/client/Client.java
@@ -1,16 +1,38 @@
 package com.dfs.client;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import com.ds.interfaces.ClientInterface;
+import com.ds.interfaces.ServerInterface;
 
 public class Client implements ClientInterface{
 	private String auth_token;
+	private String hostIP;
+	ServerInterface server;
+	int port;
+	
+	public Client(String serverHostIP, int port) throws RemoteException, NotBoundException{
+		this.hostIP = serverHostIP;
+		this.port = port;
+		
+		Registry reg = LocateRegistry.getRegistry(hostIP, port);
+		server = (ServerInterface)reg.lookup(ServerInterface.DFSERVER_UNIQUE_NAME);
+	}
 	
 	@Override
 	public void updateServerIP(String ip, int port) throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		this.hostIP = ip;
+		this.port = port;
+
+		try {
+			Registry reg = LocateRegistry.getRegistry(hostIP, port);
+			server = (ServerInterface)reg.lookup(ServerInterface.DFSERVER_UNIQUE_NAME);
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
