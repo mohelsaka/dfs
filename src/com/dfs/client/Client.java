f9989921c661912f4f5bdbf2a590dbbc95442614
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2013-05-14 14:41:21 +0200

    adding basic functions for testing from client class

--- src/com/dfs/client/Client.java
+++ src/com/dfs/client/Client.java
@@ -1,11 +1,15 @@
 package com.dfs.client;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 import com.ds.interfaces.ClientInterface;
+import com.ds.interfaces.MessageNotFoundException;
 import com.ds.interfaces.ServerInterface;
 
 public class Client implements ClientInterface{
@@ -20,6 +24,9 @@ public class Client implements ClientInterface{
 		
 		Registry reg = LocateRegistry.getRegistry(hostIP, port);
 		server = (ServerInterface)reg.lookup(ServerInterface.DFSERVER_UNIQUE_NAME);
+		
+		UnicastRemoteObject.exportObject(this, 5412);
+		server.registerClient(this);
 	}
 	
 	@Override
@@ -45,5 +52,22 @@ public class Client implements ClientInterface{
 	public String getAuthenticationToken() throws RemoteException {
 		return this.auth_token;
 	}
-
+	
+	public String read(String fileName) throws FileNotFoundException, RemoteException, IOException{
+		byte[] content = server.read(fileName).get();
+		return new String(content);
+	}
+	
+	public void write(String fileName, String content1, String content2) throws RemoteException, IOException, MessageNotFoundException{
+		long txid = server.newTxn(fileName);
+		server.write(txid, 1, content1.getBytes());
+		server.write(txid, 2, content2.getBytes());
+		server.commit(txid, 2);
+	}
+	
+	public static void main(String[] args) throws NotBoundException, FileNotFoundException, IOException, MessageNotFoundException {
+		Client c = new Client("localhost", 5555); //5892
+		c.write("geeks story1.txt", "Once upon a time,\n", "there was a geek named saka");
+		System.out.println(c.read("geeks story1.txt"));
+	}
 }
