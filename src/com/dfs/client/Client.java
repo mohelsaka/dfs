3ddb2cb694914d30684721c72434291ab9fad2ff
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2013-05-16 09:41:17 +0200

    testing some cases from client point of view

--- src/com/dfs/client/Client.java
+++ src/com/dfs/client/Client.java
@@ -7,6 +7,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -72,32 +73,50 @@ public class Client implements ClientInterface {
 	}
 
 	public static void main(String[] args) throws NotBoundException,
-			FileNotFoundException, IOException, MessageNotFoundException {
+			FileNotFoundException, IOException, MessageNotFoundException, InterruptedException {
 		Client c = new Client("localhost", 5555); // 5892
-		Scanner s = new Scanner(System.in);
-		String command = "";
-		long txid = 0;
-		int msgs = 0;
+		long txid = c.server.newTxn("mashary.txt");
+		
+		c.server.write(txid, 1, "This is a test1\n".getBytes());
+		c.server.write(txid, 2, "This is a test2\n".getBytes());
+		
+		Thread.sleep(10000);
+		
+		c.server.write(txid, 3, "This is a test3\n".getBytes());
+		c.server.write(txid, 4, "This is a test4\n".getBytes());
+		
 		try {
-			while (command != "exit") {
-				System.out.print(">>");
-				command = s.nextLine();
-				String[] chunks = command.split(" ");
-				if (chunks[0].equals("read"))
-					System.out.println(c.read(chunks[1]));
-				else if (chunks[0].equals("write")) {
-					// update: write file data then you must commit
-					txid = c.write(chunks[1], command.substring(command.indexOf(' ')));
-					if (txid == ServerInterface.INVALID_OPERATION
-							|| txid == ServerInterface.INVALID_TRANSACTION_ID)
-						System.err.println("error: data couldn't be written");
-					else
-						msgs++;
-				} else if (chunks[0].equals("commit"))
-					c.server.commit(txid, msgs);
-			}
-		} catch (NoSuchElementException e) {
-			// TODO: handle exception
+			c.server.commit(txid, 4);
+		} catch (MessageNotFoundException e) {
+			System.out.println(Arrays.toString(e.getMsgNum()));
 		}
+		
+		System.out.println(new String(c.server.read("mashary.txt").get()));
+		
+//		Scanner s = new Scanner(System.in);
+//		String command = "";
+//		long txid = 0;
+//		int msgs = 0;
+//		try {
+//			while (command != "exit") {
+//				System.out.print(">>");
+//				command = s.nextLine();
+//				String[] chunks = command.split(" ");
+//				if (chunks[0].equals("read"))
+//					System.out.println(c.read(chunks[1]));
+//				else if (chunks[0].equals("write")) {
+//					// update: write file data then you must commit
+//					txid = c.write(chunks[1], command.substring(command.indexOf(' ')));
+//					if (txid == ServerInterface.INVALID_OPERATION
+//							|| txid == ServerInterface.INVALID_TRANSACTION_ID)
+//						System.err.println("error: data couldn't be written");
+//					else
+//						msgs++;
+//				} else if (chunks[0].equals("commit"))
+//					c.server.commit(txid, msgs);
+//			}
+//		} catch (NoSuchElementException e) {
+//			// TODO: handle exception
+//		}
 	}
 }
